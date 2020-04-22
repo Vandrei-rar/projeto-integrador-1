aa4402e1de2a5d801141815a9bfca806b8d86867
Vandrei-rar <vandreiribeiro@gmail.com>
2020-04-22 15:15:34 -0300

    feat: code

--- P.I-1/P.I-1/Form1.cs
+++ P.I-1/P.I-1/Form1.cs
@@ -21,5 +21,48 @@ namespace P.I_1
         {
             System.Windows.Forms.Application.Exit();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picReset_Click(object sender, EventArgs e)
+        {
+
+            txtNumpeca.ResetText();
+            cmbTipo.SelectedIndex = -1;
+            cmbRoupa.SelectedIndex = -1;
+            rdbCama.Checked = false;
+            rdbPanos.Checked = false;
+            rdbNenhum.Checked = false;
+            rdbSim.Checked = false;
+            rdbNao.Checked = false;
+            rdbRapida.Checked = false;
+            rdbEconomica.Checked = false;
+            rdbPadrao.Checked = false;
+
+        }
+
+        private void picAcc_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cálculo realizado", "Confirmação");
+            txtNumpeca.ResetText();
+            cmbTipo.SelectedIndex = -1;
+            cmbRoupa.SelectedIndex = -1;
+            rdbCama.Checked = false;
+            rdbPanos.Checked = false;
+            rdbNenhum.Checked = false;
+            rdbSim.Checked = false;
+            rdbNao.Checked = false;
+            rdbRapida.Checked = false;
+            rdbEconomica.Checked = false;
+            rdbPadrao.Checked = false;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
