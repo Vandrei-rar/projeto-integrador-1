3a94a67a44142eca8dba4e2852794c39e3a0a992
Vandrei-rar <vandreiribeiro@gmail.com>
2020-05-08 18:47:31 -0300

    att2

--- P.I-1/P.I-1/Form1.Designer.cs
+++ P.I-1/P.I-1/Form1.Designer.cs
@@ -62,6 +62,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.erp1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxMinimizar)).BeginInit();
             this.pnlModo.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -440,12 +441,22 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(662, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox8);
@@ -527,6 +538,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ErrorProvider erp1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
