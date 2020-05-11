9e8f8abe1d7c3f765311530fdfc4cf46698a8ff0
Davi Gonçalves <davi22morais@gmail.com>
2020-05-11 14:16:44 -0300

    feat: add price calculator

--- P.I-1/P.I-1/Form1.cs
+++ P.I-1/P.I-1/Form1.cs
@@ -17,6 +17,14 @@ namespace P.I_1
             InitializeComponent();
         }
 
+        int peca = 1;
+        double tipo = 1;
+        double roupa = 1;
+        double preco = 1;
+        double enxague = 1;
+        double modopesa = 1;
+        double modoexec = 1;
+
         private void picBoxMinimizar_Click(object sender, EventArgs e)
         {
             this.FormBorderStyle = FormBorderStyle.None;
@@ -58,6 +66,12 @@ namespace P.I_1
             rdbEconomica.Checked = false;
             rdbPadrao.Checked = false;
 
+            tipo = 0;
+            roupa = 0;
+            preco = 0;
+            enxague = 0;
+            modopesa = 0;
+            modoexec = 0;
         }
 
         private void picAcc_Click(object sender, EventArgs e)
@@ -72,95 +86,183 @@ namespace P.I_1
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-            MessageBox.Show("Verificando... \nCaso os campos não estiverem preenchidos corretamente a verificação não se concluirá! Caso contrário aguarde.", "Alerta");
-
             if (txtNumpeca.Text == "")
             {
-
                 erp1.SetError(txtNumpeca, "Número de Peças Obrigatório");
 
                 txtNumpeca.BackColor = Color.FromArgb(252, 3, 3);
             }
-
-
-           else
+            else
             {
                 erp1.SetError(txtNumpeca, "");
 
                 txtNumpeca.BackColor = Color.FromName("Window");
-
             }
 
             //if (pnlModo.Text == "")
             //{
 
-              //  erp1.SetError(pnlModo, "Modo Obrigatório");
+            //  erp1.SetError(pnlModo, "Modo Obrigatório");
 
-               // pnlModo.BackColor = Color.FromArgb(252, 3, 3);
+            // pnlModo.BackColor = Color.FromArgb(252, 3, 3);
             //}
 
             //else
             //{
-              //  erp1.SetError(pnlModo, "");
+            //  erp1.SetError(pnlModo, "");
 
-              //  pnlModo.BackColor = Color.FromName("Window");
+            //  pnlModo.BackColor = Color.FromName("Window");
 
             //}
 
             if (cmbTipo.Text == "")
             {
-
                 erp1.SetError(cmbTipo, "Tipos de Roupas Obrigatório");
 
                 cmbTipo.BackColor = Color.FromArgb(252, 3, 3);
             }
-
             else
             {
                 erp1.SetError(cmbTipo, "");
 
                 cmbTipo.BackColor = Color.FromName("Window");
-
             }
 
             if (cmbRoupa.Text == "")
             {
-
                 erp1.SetError(cmbRoupa, "Classificação de Roupas Obrigatório");
 
                 cmbRoupa.BackColor = Color.FromArgb(252, 3, 3);
             }
-
             else
             {
                 erp1.SetError(cmbRoupa, "");
 
                 cmbRoupa.BackColor = Color.FromName("Window");
-
             }
 
             //if ((rdbCama.Checked = false) || (rdbPanos.Checked = false) || (rdbNenhum.Checked = false))
             //{
-              //  erp1.SetError(panel2, "Opção Obrigatória");
-              //  panel2.BackColor = Color.FromArgb(252, 3, 3);
+            //  erp1.SetError(panel2, "Opção Obrigatória");
+            //  panel2.BackColor = Color.FromArgb(252, 3, 3);
             //}
 
             //else if ((rdbCama.Checked = true) || (rdbPanos.Checked = true) || (rdbNenhum.Checked = true))
             //{
-               // erp1.SetError(panel2, "");
-               // panel2.BackColor = Color.FromName("Window");
+            // erp1.SetError(panel2, "");
+            // panel2.BackColor = Color.FromName("Window");
             //}
-        }
 
+            // Tipo
+            if  (cmbTipo.SelectedIndex == 0)
+            {
+                tipo = 2.2;
+            }
+            else if (cmbTipo.SelectedIndex == 1)
+            {
+                tipo = 1.5;
+            }
+            else if (cmbTipo.SelectedIndex == 2)
+            {
+                tipo = 1.3;
+            }
+            else if (cmbTipo.SelectedIndex == 3)
+            {
+                tipo = 2;
+            }
+            else if (cmbTipo.SelectedIndex == 4)
+            {
+                tipo = 2;
+            }
+            else if (cmbTipo.SelectedIndex == 5)
+            {
+                tipo = 1.5;
+            }
+            else if (cmbTipo.SelectedIndex == 6)
+            {
+                tipo = 1.2;
+            }
+            else if (cmbTipo.SelectedIndex == 7)
+            {
+                tipo = 1.4;
+            }
+            else if (cmbTipo.SelectedIndex == 8)
+            {
+                tipo = 2.3;
+            }
+
+            // Roupa
+            if (cmbRoupa.SelectedIndex == 0)
+            {
+                roupa = 1.8;
+            }
+            else if (cmbRoupa.SelectedIndex == 1)
+            {
+                roupa = 1.5;
+            }
+            else if (cmbRoupa.SelectedIndex == 2)
+            {
+                roupa = 1.2;
+            }
+
+            // Modo Pesado
+            if (rdbCama.Checked == true)
+            {
+                modopesa = 1.7;
+            }
+            else if (rdbPanos.Checked == true)
+            {
+                modopesa = 1.2;
+            }
+            else if (rdbNenhum.Checked == true)
+            {
+                modopesa = 1;
+            }
+
+            // Enxague
+            if (rdbSim.Checked == true)
+            {
+                enxague = 1.3;
+            }
+            else if (rdbNao.Checked == true)
+            {
+                enxague = 1;
+            }
+
+            // Modo de Execução
+            if (rdbRapida.Checked == true)
+            {
+                modoexec = 1.4;
+            }
+            else if (rdbEconomica.Checked == true)
+            {
+                modoexec = 1;
+            }
+            else if (rdbPadrao.Checked == true)
+            {
+                modoexec = 1.2;
+            }
+
+            if (txtNumpeca.Text == "")
+            {
+                peca = 0;
+            }
+            else
+            {
+                int peca = Convert.ToInt32(txtNumpeca.Text);
+            }
+
+            if (txtNumpeca.Text == "" || cmbTipo.Text == "" || cmbRoupa.Text == "")
+            {
+                MessageBox.Show("Preencha os Campos Obrigatórios!!!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                int pecaa = Convert.ToInt32(txtNumpeca.Text);
+                peca = pecaa;
+                preco = peca * tipo * roupa * modopesa * enxague * modoexec;
+                MessageBox.Show("Preço da Lavagem: R$" + preco);
+            }
+        }
     }
 }
-
-
-
-
-
-
-
-
-
